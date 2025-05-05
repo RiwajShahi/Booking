5ef28d67d70f2ca948401572b8284cdbd5009ec0
RiwajShahi <riwajshahi321@gmail.com>
2025-05-05 12:03:54 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginPage from './component/loginPage.jsx'
+
+
+const App = () => {
+  return (
+    <div className ='bg-black text-2xl'>
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='bg-amber-400'>loginPage</div>
+  )
+}
+
+export default LoginPage
